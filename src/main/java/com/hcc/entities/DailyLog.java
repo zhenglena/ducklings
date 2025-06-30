d163a9d9dceb74679da8651eabd7158461cf7449
zhenglena <zhenglena@gmail.com>
2025-06-29 21:42:27 -0500

    Add DailyLog

---
+++ src/main/java/com/hcc/entities/DailyLog.java
@@ -0,0 +1,42 @@
+package com.hcc.entities;
+
+import java.time.LocalDate;
+
+public class DailyLog {
+    private Long id;
+    private LocalDate date;
+    private Child child;
+    private List<ActivityLog> activityLog;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Child getChild() {
+        return child;
+    }
+
+    public void setChild(Child child) {
+        this.child = child;
+    }
+
+    public List<ActivityLog> getActivityLog() {
+        return activityLog;
+    }
+
+    public void setActivityLog(List<ActivityLog> activityLog) {
+        this.activityLog = activityLog;
+    }
+}
